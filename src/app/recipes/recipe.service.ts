2ef055691df5706782825018de89c148f3e8bbe3
Ian Seng <ianseng@outlook.com>
2020-03-21 18:41:35 -0400

    passing ingredients from recipe service to shopping list

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,7 @@
 import { Injectable,EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
 @Injectable({
   providedIn: 'root'
@@ -7,13 +9,17 @@ import { Recipe } from './recipe.model';
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe 1', 'This is a test', 'https://static01.nyt.com/images/2013/06/26/dining/26JPFLEX1/26JPFLEX1-articleLarge-v3.jpg'),
-    new Recipe('A Test Recipe 2', 'This is a test', 'https://static01.nyt.com/images/2013/06/26/dining/26JPFLEX1/26JPFLEX1-articleLarge-v3.jpg')
+    new Recipe('A Test Recipe 1', 'This is a test', 'https://static01.nyt.com/images/2013/06/26/dining/26JPFLEX1/26JPFLEX1-articleLarge-v3.jpg',[new Ingredient('Meat',1), new Ingredient('Egg',2)]),
+    new Recipe('A Test Recipe 2', 'This is a test', 'https://static01.nyt.com/images/2013/06/26/dining/26JPFLEX1/26JPFLEX1-articleLarge-v3.jpg',[new Ingredient('Meat',1), new Ingredient('Buns',2)])
   ];
 
-  constructor() { }
+  constructor(private slService: ShoppingListService) { }
 
   getRecipe() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredient: Ingredient[]){
+    this.slService.addIngredients(ingredient)
   }
 }
