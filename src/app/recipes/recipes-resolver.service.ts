5b388031998a4e30ff9e21a364d681bcb264984c
Ian Seng <ianseng@outlook.com>
2020-05-18 22:05:37 -0400

    section 22 create module for all recipes components

--- src/app/recipes/recipes-resolver.service.ts
+++ src/app/recipes/recipes-resolver.service.ts
@@ -4,13 +4,13 @@ import { Recipe } from './recipe.model';
 import { Injectable } from '@angular/core';
 import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 // resolver is something that angular will go through when checking the route, to make a sure a route is valid we can all this resolver to app module ts
+
 @Injectable({ providedIn: 'root' })
 export class RecipeResolverService implements Resolve<Recipe[]> {
     constructor(private dataStorageService: DataStorageService, private recipeService: RecipeService) { }
     resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-        const recipes = this.recipeService.getRecipes(); 
+        const recipes = this.recipeService.getRecipes();
         if (recipes.length === 0) {
-            
             return this.dataStorageService.fetchRecipes();
         } else {
             return recipes;
